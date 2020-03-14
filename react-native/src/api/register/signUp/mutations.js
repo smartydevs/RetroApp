36dd567101fb3899ea445636fad75879c04ddee4
Iulian Crudu <iuliancrudu@Iulians-MacBook-Pro.local>
2020-03-14 18:20:01 +0200

    Changed folder structure

--- react-native/src/api/register/signUp/mutations.js
+++ react-native/src/api/register/signUp/mutations.js
@@ -1,11 +1,9 @@
-import gql from 'graphql-tag';
+import gql from 'graphql-tag'
 
 const REGISTER_MEMBER = gql`
-    mutation registerMember($input: ProfileInput!) {
-        registerMember(input: $input)
-    }
-`;
+  mutation registerMember($input: ProfileInput!) {
+    registerMember(input: $input)
+  }
+`
 
-export  {
-  REGISTER_MEMBER
-};
+export { REGISTER_MEMBER }
