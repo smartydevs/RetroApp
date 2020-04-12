a36fec29f6995d0505e0f54b4b401ac30198e307
Romila Vlad <romilavlad2001@gmail.com>
2020-04-12 16:39:15 +0300

    small changes

--- react-native/src/containers/register/signUp/SignupComponent.js
+++ react-native/src/containers/register/signUp/SignupComponent.js
@@ -12,56 +12,56 @@ import { OS } from '../../../lib/enums'
 const { container, center, marginVertical } = ApplicationStyles
 const { bigBoldTitle, creamText, button } = Fonts.style
 
-const SignupComponent = ({onChangeEmail, onChangePassword, onChangeRepeatPassword, email,
-  password, repeatPassword, onPressFacebook, onPressSignUp}) => (
-  <KeyboardAvoidingView
-    behavior={OS.IOS === Platform.OS ? 'position' : 'height'}
-    contentContainerStyle={[container, center, styles.screen]}
-    style={[container, center, styles.screen]}
-    enabled
-  >
-    <Image
-      source={Images.logo}
-      style={styles.logo}
-      resizeMode={'contain'}
-    />
-    <Text style={[styles.welcome, bigBoldTitle, creamText]}>
-      {strings.welcome}
-    </Text>
-    <View style={styles.maxWidth}>
-      <Input
-        containerStyle={[styles.inputContainer]}
-        placeholder={strings.email}
-        onChangeText={onChangeEmail}
-        value={email}
+const SignupComponent = ({ onChangeEmail, onChangePassword, onChangeRepeatPassword, email,
+  password, repeatPassword, onPressFacebook, onPressSignUp }) => (
+    <KeyboardAvoidingView
+      behavior={OS.IOS === Platform.OS ? 'position' : 'height'}
+      contentContainerStyle={[container, center, styles.screen]}
+      style={OS.IOS === Platform.OS ? container : [container, center, styles.screen]}
+      enabled
+    >
+      <Image
+        source={Images.logo}
+        style={styles.logo}
+        resizeMode={'contain'}
       />
-      <Input
-        containerStyle={[styles.inputContainer]}
-        placeholder={strings.password}
-        autoCompleteType={'password'}
-        onChangeText={onChangePassword}
-        value={password}
-        secureTextEntry
-      />
-      <Input
-        containerStyle={[styles.inputContainer]}
-        placeholder={strings.repeatPassword}
-        autoCompleteType={'password'}
-        onChangeText={onChangeRepeatPassword}
-        value={repeatPassword}
-        secureTextEntry
-      />
-    </View>
-    <TextButton style={[styles.signup]} onPress={onPressSignUp} text={strings.signup} />
-    <Text style={[bigBoldTitle, creamText, marginVertical]}>
-      {strings.or.toUpperCase()}
-    </Text>
-    <TextButton style={[styles.facebook]} onPress={onPressFacebook} hasChildren>
-      <Ionicons name={'logo-facebook'} size={28} style={styles.facebookLogo} />
-      <Text style={[button, creamText]}>{strings.facebookLogin}</Text>
-    </TextButton>
-  </KeyboardAvoidingView>
-)
+      <Text style={[styles.welcome, bigBoldTitle, creamText]}>
+        {strings.welcome}
+      </Text>
+      <View style={styles.maxWidth}>
+        <Input
+          containerStyle={[styles.inputContainer]}
+          placeholder={strings.email}
+          onChangeText={onChangeEmail}
+          value={email}
+        />
+        <Input
+          containerStyle={[styles.inputContainer]}
+          placeholder={strings.password}
+          autoCompleteType={'password'}
+          onChangeText={onChangePassword}
+          value={password}
+          secureTextEntry
+        />
+        <Input
+          containerStyle={[styles.inputContainer]}
+          placeholder={strings.repeatPassword}
+          autoCompleteType={'password'}
+          onChangeText={onChangeRepeatPassword}
+          value={repeatPassword}
+          secureTextEntry
+        />
+      </View>
+      <TextButton style={[styles.signup]} onPress={onPressSignUp} text={strings.signup} />
+      <Text style={[bigBoldTitle, creamText, marginVertical]}>
+        {strings.or.toUpperCase()}
+      </Text>
+      <TextButton style={[styles.facebook]} onPress={onPressFacebook} hasChildren>
+        <Ionicons name={'logo-facebook'} size={28} style={styles.facebookLogo} />
+        <Text style={[button, creamText]}>{strings.facebookLogin}</Text>
+      </TextButton>
+    </KeyboardAvoidingView>
+  )
 
 SignupComponent.propTypes = {
   onChangeEmail: PropTypes.func.isRequired,
