5f678218de8ac6904d2a5a59a37624cc21bc3a2c
Iulian Crudu <iuliancrudu@Iulians-MacBook-Pro.local>
2020-08-29 16:21:51 +0300

    Small tweaks

--- microservices/api/src/modules/core/entities/Event/resolvers.js
+++ microservices/api/src/modules/core/entities/Event/resolvers.js
@@ -1,5 +1,5 @@
 import { AppUploads } from '../../../uploads/db';
-import { Categories, Events } from '../../db';
+import { Categories, Events, Reviews } from '../../db';
 import { EventService } from '../../services';
 
 export default {
@@ -7,6 +7,31 @@ export default {
     photo: ({ _id }) => {
       const event = Events.findOne(_id);
       return AppUploads.findOne(event.photoId);
+    },
+    reviews: ({ _id }) => {
+      return Reviews.createQuery({
+        $filters: {
+          eventId: _id,
+        },
+        _id: 1,
+        createdAt: 1,
+        title: 1,
+        description: 1,
+        stars: 1,
+        author: {
+          _id: 1,
+          profile: {
+            firstName: 1,
+            lastName: 1,
+            avatarId: 1,
+            avatar: {
+              _id: 1,
+              path: 1,
+              fullPath: 1,
+            },
+          },
+        },
+      }).fetch();
     },
     categories: ({ _id }) => {
       const event = Events.findOne(_id);
